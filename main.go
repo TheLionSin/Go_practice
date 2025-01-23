e0a5a62154686fbe55ae59c1eb4f131303dff079
Konstantin <crazy_boy_122@mail.ru>
2025-01-23 10:15:43 +0500

    third commint

--- main.go
+++ main.go
@@ -4,9 +4,15 @@ import "fmt"
 
 func main() {
 	fmt.Println(sum(5, 5))
+	fmt.Println(multy(5, 5))
 }
 
 func sum(first, second int) int {
 	result := first + second
+	return result
+}
+
+func multy(first, second int) int {
+	result := first * second
 	return result
 }
